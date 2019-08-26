4764a9e7a756471b45b783cf4ea82ed20188af7b
Hom Lamsal <hom.geek005@gmail.com>
2019-08-26 13:48:03 -0500

    c

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Lab10FiveCard
+
+{ class Program { static void Main(string[] args)
+        { Console.OutputEncoding = System.Text.Encoding.Unicode;
+            FiveCardDraw fiveCardDraw = new FiveCardDraw();
+            fiveCardDraw.PlayRound(); Console.ReadLine();
+
+
+        }
+    }
+}
